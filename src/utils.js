e14989afc334617f0dec97a3599aae5e9f9d4b29
Anna Ewbank <Ewbank145@gmail.com>
2024-04-23 13:08:38 +0100

    feat: navbar set-up

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 // Function to append a relative path onto the base URL of the website and return the URL
 // Allows dynamic import of images into components
 export function getImageUrl(path) {
-  return new URL(`assets/${path}`, import.meta.url).href;
+  return new URL(`/assets/${path}`, import.meta.url).href;
 }
